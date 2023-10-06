c867c168bac4615ca657c6a588d57986622eb9e9
Shehab Adel <shehabeldinadel@gmail.com>
2023-10-06 19:50:29 +0300

    Added DTO for the authentication

--- parcel-server/src/auth/auth.controller.ts
+++ parcel-server/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/signin.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -7,13 +8,13 @@ export class AuthController {
     
     @HttpCode(HttpStatus.OK)
     @Post('sender/login')
-    signInSender(@Body() signInDto: Record<string, any>) {
+    signInSender(@Body() signInDto: SignInDto) {
         return this.authService.signIn(signInDto.email, signInDto.password,'SENDER');
     }
 
     @HttpCode(HttpStatus.OK)
     @Post('biker/login')
-    signInBiker(@Body() signInDto: Record<string, any>) {
+    signInBiker(@Body() signInDto: SignInDto) {
         return this.authService.signIn(signInDto.email, signInDto.password,'BIKER');
     }
 }
